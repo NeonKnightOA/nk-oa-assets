92ceffc387373455cd25033f2a59f6b2516775d0
NeonKnightOA <openarena.arg@gmail.com>
2016-08-06 20:33:32 -0300

    Add files via upload

---
+++ tools/compile-scripts/batch/oa_nemesis.bat
@@ -0,0 +1,4 @@
+"C:\Games\OpenArena\netradiant.git-20150618\q3map2.exe" -fs_basepath "C:\Games\OpenArena" -fs_game "devoa088" -game "oa" -flares -meta -v -verboseentities "C:\Games\OpenArena\devoa088\maps\oa_nemesis.map"
+"C:\Games\OpenArena\netradiant.git-20150618\q3map2.exe" -fs_basepath "C:\Games\OpenArena" -fs_game "devoa088" -game "oa" -vis -v "C:\Games\OpenArena\devoa088\maps\oa_nemesis.map"
+"C:\Games\OpenArena\netradiant.git-20150618\q3map2.exe" -fs_basepath "C:\Games\OpenArena" -fs_game "devoa088" -game "oa" -light -dark -dirty -fast -filter -patchshadows -v -bounce 3 -gamma 1.5 -samples 3 "C:\Games\OpenArena\devoa088\maps\oa_nemesis.map"
+"C:\Games\OpenArena\netradiant.git-20150618\bspc.exe" -bsp2aas "C:\Games\OpenArena\devoa088\maps\oa_nemesis.bsp" -forcesidesvisible -optimize -grapplereach
---
+++ tools/compile-scripts/batch/oasago2.bat
@@ -0,0 +1,1 @@
+"I:\!lucho-temp\netradiant-1.5.0-20110819\bspc.exe" -bsp2aas "C:\openarena-0.8.1\baseoa\maps\oasago2.bsp" -forcesidesvisible -optimize -grapplereach
