e7468defa12814c83eec0dc9c09a3c31cb5aa3ce
NeonKnightOA <openarena.arg@gmail.com>
2017-09-21 15:00:35 -0300

    Reorganization of the repo.

--- builddevpk3.sh
+++ builddevpk3.sh
@@ -4,11 +4,12 @@
 #this script will not work.  Please check your distro's
 #documentation on how to install the zip package.
 echo Compressing...
-#zip -9r pak0.pk3 models/* -x \*svn* -x \*.map
-#zip -9r pak0.pk3 music/* -x \*svn*
-zip -9r pak0.pk3 scripts/* -x \*svn*
-#zip -9r pak0.pk3 sound/* -x \*svn*
-zip -9r pak0.pk3 textures/* -x \*svn*
+zip -9r pak7-amnk-assets.pk3 icons/* -x \*svn* -x \*.map
+zip -9r pak7-amnk-assets.pk3 models/* -x \*svn* -x \*.map
+#zip -9r pak7-amnk-assets.pk3 music/* -x \*svn*
+zip -9r pak7-amnk-assets.pk3 scripts/* -x \*svn*
+zip -9r pak7-amnk-assets.pk3 sound/* -x \*svn*
+zip -9r pak7-amnk-assets.pk3 textures/* -x \*svn*
 echo Done
 
 exit 0
