a0d14dc55089ba48dafbf6c7dcb7aed8e762d441
NeonKnightOA <openarena.arg@gmail.com>
2024-12-15 20:13:17 -0300

    Updated .ase models and their sources.

--- tools/compile-scripts/batch/!modelbuild.bat
+++ tools/compile-scripts/batch/!modelbuild.bat
@@ -1,3 +1,13 @@
-"I:\!lucho-temp\netradiant-1.5.0-20110819\q3map2.exe" -fs_basepath "D:\openarena-0.8.1" -fs_game "baseoa" -game "oa" -meta -patchmeta -subdivisions 6 "D:\openarena-0.8.1\baseoa\maps\czest3-window.map"
-"I:\!lucho-temp\netradiant-1.5.0-20110819\q3map2.exe" -fs_basepath "D:\openarena-0.8.1" -fs_game "baseoa" -game "oa" -convert ase  "D:\openarena-0.8.1\baseoa\maps\czest3-window.map"
+@echo off
+set q3map2dir=C:\Games\!Tools\NetRadiant
+set q3map2file=q3map2.exe
+set basepath=C:\Games\OpenArena 0.8.8
+
+echo %q3map2dir%
+echo %q3map2file%
+echo %basepath%
+
+for %%I in ("%basepath%\baseoa\maps\*.map") do "%q3map2dir%\%q3map2file%" -fs_basepath "%basepath%" -fs_game "baseoa" -game "quake3" -meta -patchmeta -subdivisions 6 "%%I"
+for %%I in ("%basepath%\baseoa\maps\*.map") do "%q3map2dir%\%q3map2file%" -fs_basepath "%basepath%" -fs_game "baseoa" -game "quake3" -convert ase  "%%I"
+@echo on
 pause
